f1d1b054de0136a8d447a3285600d889c747d5ce
Alhai <aly.haidara@seira-formation.fr>
2023-12-11 16:56:37 +0100

    home

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import { movieRouter } from './routes/movie-routes';
+import { actorRouter } from './routes/actor-routes';
+import mongoose from 'mongoose';
+import {Movie} from "./schema/movie-schema"
+import bodyParser from 'body-parser';
+
+const port = process.env.PORT || 1337;
 
 const app = express();
+app.use(bodyParser.json())
 
+
+mongoose.connect("mongodb+srv://ali:9Tj8er9DIeA5CK38@apiexpress.hmiffkr.mongodb.net/?retryWrites=true&w=majority", {}).then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.use('/api/movies', movieRouter)
+app.use('/api/actors', actorRouter)
+app.listen(port, () => {
+  console.log('Server is listening on port 1337');
 });
+
+export default app;
