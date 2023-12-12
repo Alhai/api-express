08b951312920421aae594e4a11efbde15083a4b2
Alhai <aly.haidara@seira-formation.fr>
2023-12-12 10:57:38 +0100

    schema

--- src/schema/movie-schema.ts
+++ src/schema/movie-schema.ts
@@ -1,5 +1,6 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
+
 const movieSchema = new mongoose.Schema({
     title:{ type : String, required : true},
     description: { type : String, required : true},
